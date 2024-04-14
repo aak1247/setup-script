df4f927ffc445e892a2a46f02e8c5a88ad2e7437
aak1247 <aak1247@126.com>
2024-04-14 13:27:18 +0800

    fix: docker & github mirror

--- env_setup.sh
+++ env_setup.sh
@@ -5,7 +5,7 @@ YELLOW="\033[33m"
 RED="\033[31m"
 NORMAL="\033[0m"
 # github mirror
-GITHUB_MIRROR="kgithub.com"
+GITHUB_MIRROR="github.com"
 # workspace
 WORKSPACE=$(pwd)
 # HTTP_PROXY="http://172.168.1.206:7890"
@@ -53,7 +53,7 @@ function checkCmdExist() {
   if [ -f "$1" ]; then
     return 0
   fi
-  if command -v "$1" &> /dev/null; then
+  if command -v "$1" &>/dev/null; then
     return 0
   else
     return 1
@@ -61,16 +61,16 @@ function checkCmdExist() {
 }
 
 function display_error() {
-	tput sgr0
-	tput setaf 1
-	echo "ERROR: $1"
-	tput sgr0
-	exit 1
+  tput sgr0
+  tput setaf 1
+  echo "ERROR: $1"
+  tput sgr0
+  exit 1
 }
 
 # check if device is in GFW or not
 function isInGFW() {
-  if command -v wget &> /dev/null; then
+  if command -v wget &>/dev/null; then
     if wget --timeout=1 -q --spider https://www.google.com/; then
       return 1
     else
@@ -132,15 +132,15 @@ function setupApt() {
       printf "${YELLOW}You are in GFW, will use aliyun mirror${NORMAL}\n"
       # if contains ubuntu, set apt mirror to aliyun
       if [[ $(getSysDist) == *"Ubuntu"* ]] || [[ $(getSysDist) == *"ubuntu"* ]]; then
-        sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak && \
-        sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list && \
-        sudo sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak &&
+          sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list &&
+          sudo sed -i 's/security.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
       fi
       # if contains debian, set apt mirror to aliyun
       if [[ $(getSysDist) == *"Debian"* ]] || [[ $(getSysDist) == *"debian"* ]]; then
-        sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak && \
-        sudo sed -i 's/deb.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list && \
-        sudo sed -i 's/security.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
+        sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak &&
+          sudo sed -i 's/deb.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list &&
+          sudo sed -i 's/security.debian.org/mirrors.aliyun.com/g' /etc/apt/sources.list
       fi
       printf "${GREEN}apt mirror successfully configured${NORMAL}\n"
     fi
@@ -157,14 +157,14 @@ function setupApps() {
   # install needed apps
   if checkNeedPrompt "install or update apps"; then
     if [[ $(getSysDist) == *"Ubuntu"* ]] || [[ $(getSysDist) == *"ubuntu"* ]] || [[ $(getSysDist) == *"Debian"* ]] || [[ $(getSysDist) == *"debian"* ]]; then
-      sudo apt-get update && \
-      sudo apt-get install -y $(getApps) build-essential libssl-dev zlib1g-dev \
-        libbz2-dev libreadline-dev libsqlite3-dev curl \
-        libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev software-properties-common
+      sudo apt-get update &&
+        sudo apt-get install -y $(getApps) build-essential libssl-dev zlib1g-dev \
+          libbz2-dev libreadline-dev libsqlite3-dev \
+          libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev software-properties-common
     elif [[ $(getSysDist) == *"CentOS"* ]]; then
       sudo yum install -y $(getApps) gcc make zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
     elif [[ $(getSysDist) == *"Arch"* ]]; then
-      sudo pacman -S --needed --noconfirm $(getApps)  base-devel openssl zlib xz tk
+      sudo pacman -S --needed --noconfirm $(getApps) base-devel openssl zlib xz tk
     elif [[ $(getSysDist) == *"Mac"* ]]; then
       brew install $(getApps) openssl readline sqlite3 xz zlib tcl-tk
     else
@@ -179,46 +179,46 @@ function setupSSH() {
   if checkNeedPrompt "configure ssh public key"; then
     if [ ! -f ~/.ssh/authorized_keys ]; then
       if [ ! -d ~/.ssh ]; then
-        mkdir -p ~/.ssh && \
-        chmod 700 ~/.ssh
+        mkdir -p ~/.ssh &&
+          chmod 700 ~/.ssh
       fi
-      touch ~/.ssh/authorized_keys && \
-      chmod 600 ~/.ssh/authorized_keys
+      touch ~/.ssh/authorized_keys &&
+        chmod 600 ~/.ssh/authorized_keys
       printf "${YELLOW}Enter your public key:${NORMAL}\n"
       read -r PUBLIC_KEY
-      echo $PUBLIC_KEY >> ~/.ssh/authorized_keys && \
-      printf "${GREEN}ssh public key successfully configured${NORMAL}\n"
+      echo $PUBLIC_KEY >>~/.ssh/authorized_keys &&
+        printf "${GREEN}ssh public key successfully configured${NORMAL}\n"
     fi
   fi
 }
 
-function setupNode(){
+function setupNode() {
   if checkCmdExist $NVM_DIR/nvm.sh; then
     printf "${GREEN}nvm is already installed${NORMAL}\n"
   else
     # install nvm
     printf "${BLUE}installing nvm into your environment${NORMAL}\n"
     if [ $IS_IN_GFW = true ]; then
-      curl -o .install_nvm.sh -L https://$GITHUB_MIRROR/nvm-sh/nvm/raw/v0.39.3/install.sh && \
-      sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" .install_nvm.sh && \
-      bash .install_nvm.sh && \
-      sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" ~/.nvm/nvm.sh
+      curl -o .install_nvm.sh -L https://$GITHUB_MIRROR/nvm-sh/nvm/raw/v0.39.3/install.sh &&
+        sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" .install_nvm.sh &&
+        bash .install_nvm.sh &&
+        sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" ~/.nvm/nvm.sh
     else
       curl -o- https://github.com/nvm-sh/nvm/raw/v0.39.3/install.sh | bash
     fi
-    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && \
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \ # This loads nvm
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" &&
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \ # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" && \ # This loads nvm bash_completion
-    echo 'export NVM_DIR=\"$NVM_DIR\"' >> ~/.zshrc && \
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.zshrc && \
-    echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.zshrc && \
-    echo "source $NVM_DIR/nvm.sh" >> ~/.zshrc && \
-    echo "source $NVM_DIR/nvm.sh" >> ~/.bashrc && \
-    printf "${GREEN}nvm successfully installed${NORMAL}\n" && \
-    source $BASHRC && \
-    nvm install $NODE_VERSION && \
-    nvm use $NODE_VERSION && \
-    npm install -g nrm yarn pnpm cnpm # install nrm / yarn / pnpm / cnpm
+    echo 'export NVM_DIR=\"$NVM_DIR\"' >>~/.zshrc &&
+      echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >>~/.zshrc &&
+      echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >>~/.zshrc &&
+      echo "source $NVM_DIR/nvm.sh" >>~/.zshrc &&
+      echo "source $NVM_DIR/nvm.sh" >>~/.bashrc &&
+      printf "${GREEN}nvm successfully installed${NORMAL}\n" &&
+      source $BASHRC &&
+      nvm install $NODE_VERSION &&
+      nvm use $NODE_VERSION &&
+      npm install -g nrm yarn pnpm cnpm # install nrm / yarn / pnpm / cnpm
     if isInGFW; then
       nrm use taobao
     fi
@@ -226,7 +226,7 @@ function setupNode(){
   fi
 }
 
-function setupPython(){
+function setupPython() {
   if checkCmdExist "$PYENV_ROOT/bin/pyenv"; then
     printf "${GREEN}pyenv is already installed${NORMAL}\n"
   else
@@ -240,19 +240,19 @@ function setupPython(){
     if [ $IS_IN_GFW = true ]; then
       sed -i "s/github.com/$REAL_GITHUB_MIRROR/g" .install_python.sh
     fi
-    bash .install_python.sh && \
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc && \
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc && \
-    echo 'eval "$(pyenv init -)"' >> ~/.zshrc && \
-    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc && \
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc && \
-    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc && \
-    echo 'eval "$(pyenv init -)"' >> ~/.bashrc && \
-    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc && \
-    sudo chmod +x pyinstall && \
-    cp pyinstall $PYENV_ROOT/bin/pyinstall && \
-    source $BASHRC && \
-    printf "${GREEN}pyenv is installed${NORMAL}\n"
+    bash .install_python.sh &&
+      echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc &&
+      echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc &&
+      echo 'eval "$(pyenv init -)"' >>~/.zshrc &&
+      echo 'eval "$(pyenv virtualenv-init -)"' >>~/.zshrc &&
+      echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc &&
+      echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc &&
+      echo 'eval "$(pyenv init -)"' >>~/.bashrc &&
+      echo 'eval "$(pyenv virtualenv-init -)"' >>~/.bashrc &&
+      sudo chmod +x pyinstall &&
+      cp pyinstall $PYENV_ROOT/bin/pyinstall &&
+      source $BASHRC &&
+      printf "${GREEN}pyenv is installed${NORMAL}\n"
   fi
   if pyenv versions | grep -q "$PYTHON_VERSION"; then
     printf "${GREEN}python $PYTHON_VERSION is already installed${NORMAL}\n"
@@ -265,10 +265,10 @@ function setupPython(){
   else
     if checkNeedPrompt "install poetry"; then
       printf "${BLUE}installing poetry${NORMAL}\n"
-      curl -sSL https://install.python-poetry.org/ | pyenv exec python - && \
-      printf "${GREEN}poetry is installed${NORMAL}\n"
-      echo 'export PATH="/home/liuyahui/.local/bin:$PATH"' >> ~/.zshrc
-      echo 'export PATH="/home/liuyahui/.local/bin:$PATH"' >> ~/.bashrc
+      curl -sSL https://install.python-poetry.org/ | pyenv exec python - &&
+        printf "${GREEN}poetry is installed${NORMAL}\n"
+      echo 'export PATH="/home/liuyahui/.local/bin:$PATH"' >>~/.zshrc
+      echo 'export PATH="/home/liuyahui/.local/bin:$PATH"' >>~/.bashrc
     fi
   fi
   # install pipenv
@@ -278,8 +278,8 @@ function setupPython(){
     if checkNeedPrompt "install pipenv"; then
       printf "${BLUE}installing pipenv${NORMAL}\n"
       pyenv shell $PYTHON_VERSION
-      pyenv exec pip install pipenv && \
-      printf "${GREEN}pipenv is installed${NORMAL}\n"
+      pyenv exec pip install pipenv &&
+        printf "${GREEN}pipenv is installed${NORMAL}\n"
     fi
   fi
   # 配置pip镜像
@@ -347,7 +347,7 @@ function setupGit() {
   fi
 }
 
-function setupGolang(){
+function setupGolang() {
   # install golang
   if checkCmdExist $HOME/.gvm/scripts/env/gvm; then
     printf "${GREEN}gvm is already installed${NORMAL}\n"
@@ -357,8 +357,8 @@ function setupGolang(){
       curl -o .install_golang.sh -L https://$REAL_GITHUB_MIRROR/moovweb/gvm/raw/master/binscripts/gvm-installer
     fi
     bash .install_golang.sh
-    echo '[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"' >> ~/.zshrc
-    if isInGFW ; then
+    echo '[[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"' >>~/.zshrc
+    if isInGFW; then
       # gvm listall set mirror
       sed -i "s/github.com/$GITHUB_MIRROR/g" ~/.gvm/scripts/listall
       sed -i '2a checkGFW' ~/.gvm/scripts/listall
@@ -366,8 +366,8 @@ function setupGolang(){
       sed -i "s/github.com/$GITHUB_MIRROR/g" ~/.gvm/scripts/install
       sed -i '2a checkGFW' ~/.gvm/scripts/install
       # set mirror for gvm install (binary)
-      echo 'export GO_BINARY_BASE_URL="$GO_MIRROR"' >> ~/.zshrc
-      echo 'export GO_BINARY_BASE_URL="$GO_MIRROR"' >> ~/.bashrc
+      echo 'export GO_BINARY_BASE_URL="$GO_MIRROR"' >>~/.zshrc
+      echo 'export GO_BINARY_BASE_URL="$GO_MIRROR"' >>~/.bashrc
     fi
     printf "${GREEN}gvm is successfully installed${NORMAL}\n"
   fi
@@ -376,19 +376,18 @@ function setupGolang(){
   else
     printf "${BLUE}installing golang${NORMAL}\n"
     [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
-    gvm install go$GO_VERSION -B && \
-    gvm use $GO_VERSION --default && \
-    go env -w GO111MODULE=on && \
-    go env -w GOPROXY=$GO_PROXY && \
-    go env -w GOPRIVATE=$GO_PRIVATE && \
-    printf "${GREEN}golang is successfully installed${NORMAL}\n" || \
-    printf "${RED}golang is not installed${NORMAL}\n"
+    gvm install go$GO_VERSION -B &&
+      gvm use $GO_VERSION --default &&
+      go env -w GO111MODULE=on &&
+      go env -w GOPROXY=$GO_PROXY &&
+      go env -w GOPRIVATE=$GO_PRIVATE &&
+      printf "${GREEN}golang is successfully installed${NORMAL}\n" ||
+      printf "${RED}golang is not installed${NORMAL}\n"
     # TODO: goprivate Config
   fi
 }
 
-
-function setupJava(){
+function setupJava() {
   # install java
   if checkCmdExist java; then
     printf "${GREEN}java is already installed${NORMAL}\n"
@@ -412,22 +411,21 @@ function setupJava(){
   # TODO: maven / maven mirror / gradle / gradle mirror
 }
 
-
-function setupRust(){
+function setupRust() {
   # install rust
   if checkCmdExist rustc; then
     printf "${GREEN}rust is already installed${NORMAL}\n"
   else
     printf "${BLUE}installing rust${NORMAL}\n"
-    curl https://sh.rustup.rs -sSf | sh && \
-    echo '. "$HOME/.cargo/env"' >> ~/.zshrc && \
-    echo '. "$HOME/.cargo/env"' >> ~/.bashrc && \
-    touch $HOME/.cargo/.config
+    curl https://sh.rustup.rs -sSf | sh &&
+      echo '. "$HOME/.cargo/env"' >>~/.zshrc &&
+      echo '. "$HOME/.cargo/env"' >>~/.bashrc &&
+      touch $HOME/.cargo/.config
     echo '[source.crates-io]
 registry = "https://github.com/rust-lang/crates.io-index"
 replace-with = "ustc"
 [source.ustc]
-registry = "git://mirrors.ustc.edu.cn/crates.io-index"' >> $HOME/.cargo/.config
+registry = "git://mirrors.ustc.edu.cn/crates.io-index"' >>$HOME/.cargo/.config
     printf "${GREEN}rust is successfully installed${NORMAL}\n"
   fi
 }
@@ -440,23 +438,20 @@ function setUpZsh() {
     return 0
   fi
   if [ $IS_IN_GFW = true ]; then
-    sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" ./install_zsh.sh && \
-    sed -i "s/https:\/\/raw.githubusercontent.com/https:\/\/raw.$GITHUB_MIRROR/g" ./install_zsh.sh
+    sed -i "s/https:\/\/github.com/https:\/\/$GITHUB_MIRROR/g" ./install_zsh.sh &&
+      sed -i "s/https:\/\/raw.githubusercontent.com/https:\/\/raw.$GITHUB_MIRROR/g" ./install_zsh.sh
   fi
   bash ./install_zsh.sh
 }
 
 function setUpDocker() {
   # check docker is installed
-  if command -v docker &> /dev/null; then
+  if command -v docker &>/dev/null; then
     printf "${GREEN}docker is already installed${NORMAL}\n"
   else
     printf "${BLUE}installing docker into your environment${NORMAL}\n"
-    if [ $IS_IN_GFW = true ]; then
-      curl -fsSL https://get.daocloud.io/docker | bash
-    else
-      curl -sSL https://get.daocloud.io/docker | bash
-    fi
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
     sudo chmod 666 /var/run/docker.sock
     sudo usermod -aG docker $USER
     sudo systemctl enable docker
@@ -464,16 +459,16 @@ function setUpDocker() {
     printf "${GREEN}docker is installed${NORMAL}\n"
   fi
   # check docker-compose is installed
-  if command -v docker-compose &> /dev/null; then
+  if command -v docker-compose &>/dev/null; then
     printf "${GREEN}docker-compose is already installed${NORMAL}\n"
   else
     printf "${BLUE}installing docker-compose into your environment${NORMAL}\n"
     if [ $IS_IN_GFW = true ]; then
-      sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
-      sudo chmod +x /usr/local/bin/docker-compose
+      sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &&
+        sudo chmod +x /usr/local/bin/docker-compose
     else
-      sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-      sudo chmod +x /usr/local/bin/docker-compose
+      sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+        sudo chmod +x /usr/local/bin/docker-compose
     fi
     printf "${GREEN}docker-compose is installed${NORMAL}\n"
   fi
@@ -487,12 +482,12 @@ function setUpTmux() {
     cd $WORKSPACE
     return 0
   fi
-  printf "${BLUE}configuring tmux${NORMAL}\n" && \
-  git clone https://$REAL_GITHUB_MIRROR/aak1247/.tmux.git && \
-  ln -s -f .tmux/.tmux.conf && \
-  cp .tmux/.tmux.conf.local . && \
-  printf "${GREEN}tmux is configured${NORMAL}\n" && \
-  cd $WORKSPACE
+  printf "${BLUE}configuring tmux${NORMAL}\n" &&
+    git clone https://$REAL_GITHUB_MIRROR/aak1247/.tmux.git &&
+    ln -s -f .tmux/.tmux.conf &&
+    cp .tmux/.tmux.conf.local . &&
+    printf "${GREEN}tmux is configured${NORMAL}\n" &&
+    cd $WORKSPACE
 }
 
 function setupNix() {
@@ -501,27 +496,27 @@ function setupNix() {
     return 0
   else
     printf "${BLUE}installing nix${NORMAL}\n"
-    sh <(curl -L https://nixos.org/nix/install) --daemon && \
-    if [ $? -eq 0 ]; then
-      printf "${GREEN}nix is successfully installed${NORMAL}\n"
-    else
-      printf "${RED}nix is not installed${NORMAL}\n"
-    fi
+    sh <(curl -L https://nixos.org/nix/install) --daemon &&
+      if [ $? -eq 0 ]; then
+        printf "${GREEN}nix is successfully installed${NORMAL}\n"
+      else
+        printf "${RED}nix is not installed${NORMAL}\n"
+      fi
   fi
 }
 
-source $BASHRC && \
-setupApt && \
-setupApps && \
-setupSSH && \
-setUpZsh && \
-setupNode && \
-setupGit && \
-setUpDocker && \
-setUpTmux && \
-setupPython && \
-setupGolang && \
-setupJava && \
-setupRust && \
-setupNix && \
-printf "${GREEN}Congratulations, your env is ready${NORMAL}\n"
+source $BASHRC &&
+  setupApt &&
+  setupApps &&
+  setupSSH &&
+  setUpZsh &&
+  setupNode &&
+  setupGit &&
+  setUpDocker &&
+  setUpTmux &&
+  setupPython &&
+  setupGolang &&
+  setupJava &&
+  setupRust &&
+  setupNix &&
+  printf "${GREEN}Congratulations, your env is ready${NORMAL}\n"
